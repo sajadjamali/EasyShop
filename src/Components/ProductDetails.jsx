56092a037db2027fe592f591c4be7949ec189255
sajadjamali <125208168+sajadjamali@users.noreply.github.com>
2023-11-21 21:41:53 +0330

    changing background color

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -11,23 +11,23 @@ const ProductDetails = () => {
     const item = products.find(p => p.id == productID);
 
     return (
-        <div className=" bg-indigo-400 h-full xl:h-screen pb-10">
+        <div className=" bg-indigo-300 h-full xl:h-screen pb-10">
             <Header />
             {item ?
                 <section className="flex flex-col items-center justify-center px-4 pt-8 xl:flex-row">
-                    <img src={item.image} className="h-96 me-5 xl:w-4/12 rounded-md shadow-gray-500" alt="not found" />
-                    <div className="text-justify space-y-6 bg-indigo-400 mt-5 font-bold">
+                    <img src={item.image} className="h-96 me-5 xl:w-4/12 rounded-md" alt="not found" />
+                    <div className="text-justify space-y-6 bg-indigo-300 mt-5 font-bold">
                         <p className="text-rose-900 text-center text-xl">{item.title}</p>
                         <hr className="bg-red-800 h-1" />
-                        <p className="text-center"><span className="text-purple-900">Category:</span> {item.category}</p>
+                        <p className="text-center"><span className="text-purple-950">Category:</span> {item.category}</p>
                         <hr className="bg-red-800 h-1" />
-                        <p className="text-center"><span className="text-purple-900">Price:</span> {item.price}$</p>
+                        <p className="text-center"><span className="text-purple-950">Price:</span> {item.price}$</p>
                         <hr className="bg-red-800 h-1" />
-                        <p className="text-center"><span className="text-purple-900">Rate:</span> {item.rating.rate}</p>
+                        <p className="text-center"><span className="text-purple-950">Rate:</span> {item.rating.rate}</p>
                         <hr className="bg-red-800 h-1" />
-                        <p className="text-center"><span className="text-purple-900">Count:</span> {item.rating.count}</p>
+                        <p className="text-center"><span className="text-purple-950">Count:</span> {item.rating.count}</p>
                         <hr className="bg-red-800 h-1" />
-                        <p className="font-normal"><span className="text-purple-900 font-bold">Description:</span> {item.description}</p>
+                        <p className="font-normal"><span className="text-purple-950 font-bold">Description:</span> {item.description}</p>
                     </div>
                 </section>
                 : <Spinner />}
